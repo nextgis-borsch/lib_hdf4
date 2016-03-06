c917de3f347cd43fb5c56cf2b73cc30d0c4533ee
Dmitry Baryshnikov <bishop.dev@gmail.com>
2016-03-07 02:08:07 +0300

    Disable update step for external project

--- cmake/FindExtProject.cmake
+++ cmake/FindExtProject.cmake
@@ -168,6 +168,7 @@ function(find_extproject name)
     ExternalProject_Add(${name}_EP
         GIT_REPOSITORY ${EP_URL}/${repo_name}
         CMAKE_ARGS ${find_extproject_CMAKE_ARGS}
+        UPDATE_DISCONNECTED 1
     )
         
     find_package(Git)
@@ -242,7 +243,9 @@ function(find_extproject name)
         include_directories(${EP_BASE}/Install/${name}_EP/include/${inc})
     endforeach ()    
     
-    install( DIRECTORY ${EP_BASE}/Install/${name}_EP/ DESTINATION ${CMAKE_INSTALL_PREFIX} )
+    install( DIRECTORY ${EP_BASE}/Install/${name}_EP/ 
+             DESTINATION ${CMAKE_INSTALL_PREFIX} 
+             COMPONENT libraries)
         
     set(EXPORTS_PATHS ${EXPORTS_PATHS} PARENT_SCOPE)
 endfunction()
